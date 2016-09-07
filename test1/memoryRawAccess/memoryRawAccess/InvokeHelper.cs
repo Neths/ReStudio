198ffefa91c48b2382f3e9640817d3783b554b68
Neths <vacher.michel@gmail.com>
2016-09-07 16:30:57 +0200

    new test memoryRawAccess

---
+++ test1/memoryRawAccess/memoryRawAccess/InvokeHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace memoryRawAccess
+{
+    public static class InvokeHelper
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        //[DllImport("Kernel32.Dll")]
+        //public static extern bool ReadProcessMemory(
+        //    IntPtr processHandle, 
+        //    ulong address, 
+        //    byte[] buffer, 
+        //    uint size, 
+        //    ref uint bytesRead);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int dwSize,
+            out IntPtr lpNumberOfBytesRead);
+
+
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //static extern bool ReadProcessMemory(
+        //    IntPtr hProcess,
+        //    IntPtr lpBaseAddress,
+        //    IntPtr lpBuffer,
+        //    int dwSize,
+        //    out IntPtr lpNumberOfBytesRead);
+
+
+        [DllImport("Kernel32.Dll")]
+        public static extern uint VirtualQueryEx(IntPtr processHandle, ulong address, 
+            ref MEMORY_BASIC_INFORMATION memInfo, int memInfoLength);
+
+        [DllImport("Kernel32.Dll")]
+        public static extern uint GetLastError();
+        [DllImport("Kernel32.Dll")]
+        public static extern uint FormatMessage(uint dwFlags, uint lpSource, uint dwMessageId, uint dwLanguageId, byte[] lpBuffer, uint nSize, uint arguments);
+    }
+}
---
+++ test1/memoryRawAccess/memoryRawAccess/MemoryRegion.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace memoryRawAccess
+{
+    public class MemoryRegion
+    {
+        public ulong Address;
+        public ulong Size;
+        public AllocationProtect Protection;
+        public MEMORY_BASIC_INFORMATION Information;
+        public PageState State;
+
+        private byte[] _buffer;
+
+        public MemoryRegion(IntPtr processHandle)
+        {
+            ProcessHandle = processHandle;
+        }
+
+        public IntPtr ProcessHandle { get; private set; }
+
+        public byte[] GetBytes()
+        {
+            if (Size >= 0x8000000)
+                return new byte[0];
+
+            var buffer = new byte[Size];
+            IntPtr bytesRead;
+
+            if (InvokeHelper.ReadProcessMemory(ProcessHandle, (IntPtr)Address, buffer, buffer.Length, out bytesRead))
+                return buffer;
+
+            Console.WriteLine($"[0x{Address:X8}]Error reading memory: {InvokeHelper.GetLastError()}");
+            var buf = new byte[256];
+            var bytes = InvokeHelper.FormatMessage(0x1000, 0, 6, 0, buf, 255, 0);
+            if (bytes == 0)
+            {
+                Console.WriteLine($"[0x{Address:X8}]Error with FormatMessage(): {InvokeHelper.GetLastError()}");
+            }
+            else
+            {
+                Console.Write($"[0x{Address:X8}]Error message: {System.Text.Encoding.ASCII.GetString(buf, 0, (int) bytes)}");
+            }
+            return null;
+        }
+
+        public IEnumerable<ulong> FindBytes(byte[] searchBytes)
+        {
+            if (_buffer == null)
+                _buffer = GetBytes();
+
+            if (_buffer == null || searchBytes == null || searchBytes.Length == 0)
+            {
+                return new ulong[0];
+            }
+            var list = new List<ulong>();
+
+            uint i;
+            for (i = 0; i < _buffer.Length - searchBytes.Length; i++)
+            {
+                if (_buffer[i] == searchBytes[0])
+                {
+                    uint j;
+                    for (j = 1; j < searchBytes.Length; j++)
+                    {
+                        if (_buffer[i + j] != searchBytes[j])
+                            break;
+                    }
+
+                    if (j >= searchBytes.Length)
+                    {
+                        list.Add(i + Address);
+                        if (list.Count >= 32768)
+                            break;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
