66dfd286d44980ceb5ec1c04bb26f99fa4397107
Neths <vacher.michel@gmail.com>
2016-10-07 15:16:19 +0200

    project for ui testing

---
+++ test1/SniperForm/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SniperForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblH = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblV = new System.Windows.Forms.Label();
+            this.txtOffset = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 108);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(83, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(83, 213);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(75, 20);
+            this.txtR.TabIndex = 5;
+            this.txtR.Text = "150";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "G";
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(83, 239);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(75, 20);
+            this.txtG.TabIndex = 7;
+            this.txtG.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "B";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(83, 265);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(75, 20);
+            this.txtB.TabIndex = 9;
+            this.txtB.Text = "10";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Location = new System.Drawing.Point(178, 216);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(15, 13);
+            this.lblH.TabIndex = 11;
+            this.lblH.Text = "H";
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Location = new System.Drawing.Point(178, 242);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(15, 13);
+            this.lblS.TabIndex = 12;
+            this.lblS.Text = "H";
+            // 
+            // lblV
+            // 
+            this.lblV.AutoSize = true;
+            this.lblV.Location = new System.Drawing.Point(178, 268);
+            this.lblV.Name = "lblV";
+            this.lblV.Size = new System.Drawing.Size(15, 13);
+            this.lblV.TabIndex = 13;
+            this.lblV.Text = "H";
+            // 
+            // txtOffset
+            // 
+            this.txtOffset.Location = new System.Drawing.Point(86, 356);
+            this.txtOffset.Name = "txtOffset";
+            this.txtOffset.Size = new System.Drawing.Size(158, 20);
+            this.txtOffset.TabIndex = 14;
+            this.txtOffset.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 536);
+            this.Controls.Add(this.txtOffset);
+            this.Controls.Add(this.lblV);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblV;
+        private System.Windows.Forms.TextBox txtOffset;
+    }
+}
+
---
+++ test1/SniperForm/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+
+namespace SniperForm
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> _img;
+        private int colorRange = 0;
+        private int offset = 5;
+        Image<Gray, Byte> imgCleand = null;
+
+        private Tesseract ocr;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ocr = new Tesseract();
+            ocr.SetVariable("tessedit_char_whitelist", "1234567890");
+            ocr.Init(@"D:\", "eng", OcrEngineMode.TesseractOnly);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _img = new Image<Bgr, byte>(@"d:\raw\classified\test.jpg");
+
+            pictureBox1.Image = _img.ToBitmap();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var a = new Image<Gray, byte>(new Size
+            //{
+            //    Height = _img.Height,
+            //    Width = _img.Width
+            //});
+
+            //CvInvoke.CvtColor(_img, a, ColorConversion.Bgr2Hsv);
+
+            imgCleand = _img.InRange(new Bgr(int.Parse(txtB.Text) - int.Parse(txtOffset.Text), int.Parse(txtG.Text) - int.Parse(txtOffset.Text), int.Parse(txtR.Text) - int.Parse(txtOffset.Text)), 
+                new Bgr(int.Parse(txtB.Text) + int.Parse(txtOffset.Text), int.Parse(txtG.Text) + int.Parse(txtOffset.Text), int.Parse(txtR.Text) + int.Parse(txtOffset.Text)));
+
+
+            pictureBox1.Image = imgCleand.ToBitmap();
+
+            
+
+
+
+            //using (Image<Hsv, byte> hsv = _img.Convert<Hsv, byte>())
+            //{
+            //    // 2. Obtain the 3 channels (hue, saturation and value) that compose the HSV image
+            //    Image<Gray, byte>[] channels = hsv.Split();
+
+            //    try
+            //    {
+            //        //channels[0].InRange(new Gray(40), new Gray(60));
+            //        //// 3. Remove all pixels from the hue channel that are not in the range [40, 60]
+            //        //CvInvoke.cvInRangeS(channels[0], new Gray(40).MCvScalar, new Gray(60).MCvScalar, channels[0]);
+
+
+
+
+
+            //        var a = channels[0].InRange(new Gray(Color.Red.GetHue() - 0), new Gray(Color.Red.GetHue() + 0)).ToBitmap();
+
+            //        // 4. Display the result
+            //        pictureBox1.Image = CvInvoke.cv;
+            //    }
+            //    finally
+            //    {
+            //        channels[1].Dispose();
+            //        channels[2].Dispose();
+            //    }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ocr.Recognize(imgCleand);
+
+            var sb = new StringBuilder();
+            ocr.GetText().Where(char.IsDigit).ToList().ForEach(c => sb.Append(c));
+            lblH.Text = sb.ToString();
+        }
+    }
+}
